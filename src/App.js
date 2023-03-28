bef2dc3354ac3385cc70446d56d841cd0d3cbee9
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-03-28 16:19:49 +0300

    Display the Omniva API.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,25 @@
-import React, {useState, useRef, useEffect} from "react";
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    const [tooted, setTooted] = useState([]);
-    const idRef = useRef();
-    const nameRef = useRef();
-    const priceRef = useRef();
-    const isActiveRef = useRef();
+    const [pakiautomaadid, setPakiautomaadid] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:3000/tooted")
+        fetch("http://localhost:8080/parcel-machines")
             .then(res => res.json())
-            .then(json => setTooted(json));
+            .then(json => setPakiautomaadid(json));
     }, []);
-
-    const kustuta = (index) => {
-        fetch("http://localhost:3000/kustuta-toode/" + index, {"method": "delete"})
-            .then(res => res.json())
-            .then(json => setTooted(json))
-    }
-
-    const lisa = () => {
-        const uusToode = {
-            "id": idRef.current.value,
-            "nimi": nameRef.current.value,
-            "hind": priceRef.current.value,
-            "aktiivne": isActiveRef.current.checked
-        };
-        fetch(`http://localhost:3000/lisa-toode/`, {"method": "post", "body": JSON.stringify(uusToode)})
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }
-
-    const dollariteks = () => {
-        const kurss = 1.1;
-        fetch("http://localhost:3000/hind-dollaritesse/" + kurss, {"method": "patch"})
-            .then(res => res.json())
-            .then(json => setTooted(json));
-
-    }
 
     return (
         <div className="App">
-            <div className="add">
-                <label>ID</label> <br />
-                <input ref={idRef} type="number" /> <br />
-                <label>Nimi</label> <br />
-                <input ref={nameRef} type="text" /> <br />
-                <label>Hind</label> <br />
-                <input ref={priceRef} type="number" /> <br />
-                <label>Aktiivne</label> <br />
-                <input ref={isActiveRef} type="checkbox" /> <br />
-                <button onClick={() => lisa()}>Lisa</button>
-            </div>
-            {tooted.map((toode, index) =>
-                <div key={Math.random()} className="product">
-                    <div>{toode._id}</div>
-                    <div>{toode._name}</div>
-                    <div>{toode.price}</div>
-                    <button onClick={() => kustuta(index)}>X</button>
-                </div>)}
-            <button onClick={() => dollariteks()}>Muuda dollariteks</button>
+            <select>
+                {pakiautomaadid.map(automaat =>
+                    <option>
+                        {automaat.NAME}
+                    </option>)}
+            </select>
         </div>
     );
 }
-
 
 export default App;
